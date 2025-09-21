12fc84ef19d22a242a1de9308b54bfa06f5e705e
Reysan <michalreyman@gmail.com>
2025-09-21 21:55:08 +0200

    section 2 - repository

---
+++ section2/accounts/src/main/java/com/eazybytes/accounts/entity/Accounts.java
@@ -0,0 +1,20 @@
+package com.eazybytes.accounts.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter @Setter @ToString @AllArgsConstructor @NoArgsConstructor
+public class Accounts extends BaseEntity {
+
+    @Column(name = "customer_id")
+    private Long customerId;
+
+    @Id
+    private Long  accountNumber;
+
+    @Column(name = "account_type")
+    private String accountType;
+
+    private String branchAddress;
+}
---
+++ section2/accounts/src/main/java/com/eazybytes/accounts/entity/BaseEntity.java
@@ -0,0 +1,25 @@
+package com.eazybytes.accounts.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass // This annotation indicates that this class is a base class for JPA entities
+@Getter
+@Setter
+@ToString
+public class BaseEntity {
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+    @Column(updatable = false)
+    private String createdBy;
+    @Column(insertable = false)
+    private LocalDateTime updatedAt;
+    @Column(insertable = false)
+    private String updatedBy;
+}
+
