f4e7ad2648996000087a5d83b7166e322f7e3492
Reysan <michalreyman@gmail.com>
2025-09-22 18:45:59 +0200

    section 2 - dto

---
+++ section2/accounts/src/main/java/com/eazybytes/accounts/dto/AccountsDto.java
@@ -0,0 +1,18 @@
+package com.eazybytes.accounts.dto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+public class AccountsDto {
+
+
+    private Long customerId;
+
+    private Long  accountNumber;
+
+    private String accountType;
+
+    private String branchAddress;
+}
