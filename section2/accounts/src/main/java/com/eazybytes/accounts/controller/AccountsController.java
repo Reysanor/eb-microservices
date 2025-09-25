430bfcbb03277b301e82be6ab38072593935baba
Reysan <michalreyman@gmail.com>
2025-09-25 16:57:26 +0200

    section 2 - fetch

--- section2/accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
+++ section2/accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
@@ -9,10 +9,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * AccountsController handles HTTP requests related to account operations.
@@ -32,5 +29,12 @@ public class AccountsController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
+    }
+
+
+    @GetMapping("/fetch")
+    public ResponseEntity<CustomerDto> fetchAccountDetails(@RequestParam String mobileNumber) {
+        CustomerDto customerDto = iAccountsService.fetchAccount(mobileNumber);
+        return ResponseEntity.status(HttpStatus.OK).body(customerDto);
     }
 }
--- section2/accounts/src/main/java/com/eazybytes/accounts/dto/CustomerDto.java
+++ section2/accounts/src/main/java/com/eazybytes/accounts/dto/CustomerDto.java
@@ -5,11 +5,11 @@ import lombok.Data;
 @Data
 public class CustomerDto {
 
-
-
     private String name;
 
     private String email;
 
     private String mobileNumber;
+
+    private AccountsDto accountsDto;
 }
