c7cda75bba78ee2eef661df66ea7f7e517471d0d
Reysan <michalreyman@gmail.com>
2026-02-01 20:41:36 +0100

    section 8 - eurekaserver

--- eurekaserver/src/main/java/com/eazybytes/eurekaserver/EurekaserverApplication.java
+++ eurekaserver/src/main/java/com/eazybytes/eurekaserver/EurekaserverApplication.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+// To access the Eureka Server dashboard, navigate to http://localhost:8070
+// Eureka Server acts as a service registry that holds information about all available microservice instances.
+// It enables automatic registration of services and simplifies inter-service communication without hardcoding IP addresses or hostnames.
 @SpringBootApplication
-@EnableEurekaServer
+@EnableEurekaServer // Enable Eureka Server functionality in this Spring Boot application
 public class EurekaserverApplication {
 
 	public static void main(String[] args) {
